573ae47f380b1dfa21b7b4a13e318ca8c97c392a
Czarek <sznurek05@gmail.com>
2018-10-27 11:51:54 +0200

    Dodaj pliki projektów.

---
+++ KCK_Projekt1_CezarySzmurlo_Tetris/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCK_Projekt1_CezarySzmurlo_Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
